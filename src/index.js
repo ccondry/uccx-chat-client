300457ceb9ece31ab9e5055b33efe18d775fad74
Coty Condry <ccondry@cisco.com>
2018-07-12 16:15:31 -0500

    move index to test move lib to index

--- src/index.js
+++ src/index.js
@@ -1,72 +1,240 @@
-const chatLib = require('./lib')
+const request = require('request-promise-native')
+const queryString = require('query-string')
+const parseXmlString = require('./parse-xml')
 
-let messageInterval
+// strip invalid xml characters including gifs, images, emojis, etc
+function stripNonValidXMLCharacters(text){
+  const out = [] // Used to hold the output.
+  if (!text || text === '') {
+    return ''
+  }
 
-const handlers = {
+  for ( var i = 0; i < text.length; i++) {
+    const current = text.charCodeAt(i)
+    if ((current == 0x9) ||
+    (current == 0xA) ||
+    (current == 0xD) ||
+    ((current >= 0x20) && (current <= 0xD7FF)) ||
+    ((current >= 0xE000) && (current <= 0xFFFD)) ||
+    ((current >= 0x10000) && (current <= 0x10FFFF)))
+    out.push(text.charAt(i))
+  }
+  return out.join('')
+}
+
+// default handlers, to prevent reference errors
+const defaultHandlers = {
   onMessageEvent (from, message) {
-    console.log(from, 'said', message)
   },
   onStatusEvent (status, detail) {
-    console.log('status event', status, detail)
   },
   onPresenceEvent (from, status) {
-    console.log('presence event', from, status)
   },
   onPresenceJoined (from) {
-    console.log('presence joined', from)
-    console.log('participant joined. start sending messages as customer.')
-    // check that there is not a current interval set up for sending messages
-    if (messageInterval) {
-      // clear the current interval
-      clearInterval(messageInterval)
-    }
-    // set up new interval to send messages to agent every 5 seconds
-    messageInterval = setInterval(() => {
-      this.sendMessage('hi from customer')
-    }, 5000)
   },
   onPresenceLeft (from) {
-    console.log('presence left', from)
   },
   onLastParticipantLeft () {
-    console.log('last participant left')
   },
   onTypingEvent (from, status) {
-    console.log('typing event', from, status)
   },
   onTypingStart (from) {
-    console.log(from, 'is typing')
   },
   onTypingStop (from) {
-    console.log(from, 'stopped typing')
   },
   onOtherEvent (type, ev) {
-    console.log('other event', type, ev)
   },
   onAgentTimeout (message) {
-    console.log('agent timeout', message)
   },
   onChatCreated () {
-    console.log('chat created')
   },
   onStopPolling () {
-    console.log('polling stopped')
-    // clear our automated cutomer message interval
-    clearInterval(messageInterval)
   },
   onSessionExpired () {
-    console.log('session expired')
   }
 }
 
-const chat = new chatLib({
-  urlBase: 'https://sm2-uccx.dcloud.cisco.com/ccp',
-  form: 100000,
-  csq: 'Chat_Csq28',
-  title: 'Facebook Messenger',
-  customerName: 'Facebook Messenger',
-  author: 'Coty Condry',
-  handlers
-})
+module.exports = class UccxChatClient {
+  constructor (params) {
+    // the cookie used for REST requests for this session
+    this.jar = request.jar()
+    // probably no longer needed
+    this.jsessionid = null
+    // beginning event ID
+    this.lastEventId = 0
+    // events polling interval
+    this.eventsInterval = null
+    // number of participants (agents)
+    this.participants = 0
+    // chat form ID - a number >= 100000
+    this.form = params.form
+    // base SocialMiner url, like https://sm2-uccx.dcloud.cisco.com/ccp
+    this.urlBase = params.urlBase
+    // CSQ ID in UCCX
+    this.csq = params.csq
+    // title of the chat
+    this.title = params.title || ''
+    // customer's name
+    this.customerName = params.customerName || this.title
+    // customer's email
+    this.customerEmail = params.customerEmail || ''
+    // customer's phone
+    this.customerPhone = params.customerPhone || ''
+    // author
+    this.author = params.author || this.customerName
 
-chat.start()
+    // callback handlers
+    this.handlers = Object.assign(defaultHandlers, params.handlers)
+  }
+
+  start () {
+    console.log('creating UCCX chat client at', this.urlBase)
+    // start a new chat session on UCCX/SocialMiner
+    const qs = {
+      author: this.author,
+      title: this.title,
+      extensionField_Name: this.customerName,
+      extensionField_Email: this.customerEmail,
+      extensionField_PhoneNumber: this.customerPhone,
+      extensionField_ccxqueuetag: this.csq
+    }
+    request({
+      jar: this.jar,
+      url: `${this.urlBase}/chat/${this.form}/redirect`,
+      resolveWithFullResponse: true,
+      simple: false,
+      followRedirect: false,
+      qs
+    }).then(response => {
+      this.startPolling()
+    }).catch(e => {
+      console.error('error', e.message)
+      // make sure we stop polling
+      this.stopPolling()
+    })
+  }
+
+  startPolling () {
+    // poll events every 5 seconds
+    this.eventsInterval = setInterval(() => {
+      // check for events for this session
+      request({
+        jar: this.jar,
+        url: `${this.urlBase}/chat`,
+        qs: {
+          eventid: this.lastEventId,
+          // use all = true to also get customer messages
+          all: false
+        }
+      }).then(events => {
+        // parse xml events string into JSON
+        return parseXmlString(events)
+      }).then(jsonData => {
+        // parse the events in the json
+        this.parseEvents(jsonData.chatEvents)
+      }).catch(e => {
+        console.log('session expired?', e)
+        this.stopPolling()
+      })
+    }, 5000)
+  }
+
+  stopPolling () {
+    // clear the current intervals
+    clearInterval(this.eventsInterval)
+    this.handlers.onStopPolling.call(this)
+  }
+
+  parseEvents (events) {
+    const eventTypes = Object.keys(events)
+    for (const eventType of eventTypes) {
+      for (const ev of events[eventType]) {
+        this.processEvent(eventType, ev)
+      }
+    }
+  }
+
+  processEvent (type, ev) {
+    // console.log('event', ev)
+    // get event ID
+    const id = parseInt(ev.id)
+    if (id > this.lastEventId) {
+      // increment eventid
+      this.lastEventId = id
+    }
+    if (type === 'MessageEvent') {
+      // message events
+      this.handlers.onMessageEvent.call(this, ev.from[0], decodeURIComponent(ev.body[0].replace(/\+/g, ' ')))
+      // console.log(ev)
+    } else if (type === 'StatusEvent') {
+      // status events
+      const status = ev.status[0]
+      const detail = ev.detail ? ev.detail[0] : ''
+      this.handlers.onStatusEvent.call(this, status, detail)
+      switch (status) {
+        case 'chat_timedout_waiting_for_agent': {
+          this.handlers.onAgentTimeout.call(this, detail)
+          break
+        }
+        case 'chat_ok': {
+          this.handlers.onChatCreated.call(this)
+          break
+        }
+      }
+    } else if (type === 'PresenceEvent') {
+      this.handlers.onPresenceEvent.call(this, ev.from[0], ev.status[0])
+      // keep track of the number of participants
+      if (ev.status[0] === 'joined') {
+        this.handlers.onPresenceJoined.call(this, ev.from[0])
+        this.participants++
+      } else if (ev.status[0] === 'left') {
+        this.handlers.onPresenceLeft.call(this, ev.from[0])
+        this.participants--
+        // if the last participant left, the chat has ended. stop polling.
+        if (this.participants < 1) {
+          this.handlers.onLastParticipantLeft.call(this)
+          // clear the current intervals
+          this.stopPolling()
+        }
+      }
+      console.log('participants =', this.participants)
+    } else if (type === 'TypingEvent') {
+      this.handlers.onTypingEvent.call(this, ev.from[0], ev.status[0])
+      switch (ev.status[0]) {
+        case 'composing': {
+          this.handlers.onTypingStart.call(this, ev.from[0])
+          break
+        }
+        case 'paused': {
+          this.handlers.onTypingStop.call(this, ev.from[0])
+          break
+        }
+      }
+    } else {
+      this.handlers.onOtherEvent.call(this, type, ev)
+    }
+  }
+
+  sendMessage (message) {
+    const cleanMessage = stripNonValidXMLCharacters(message)
+    // send a message as the customer
+    request({
+      jar: this.jar,
+      method: 'PUT',
+      url: `${this.urlBase}/chat`,
+      headers: {
+        'Content-Type': 'application/xml'
+      },
+      body: `<Message><body>${cleanMessage}</body></Message>`
+    }).then(response => {
+      console.log('customer message sent.')
+    }).catch(e => {
+      console.error('failed to send message as customer', e.message)
+      if (e.statusCode === 404) {
+        // session expired - stop polling
+        this.handlers.onSessionExpired.call(this)
+        this.stopPolling()
+      }
+    })
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,143 +1,72 @@
-const request = require('request-promise-native')
-const queryString = require('query-string')
-const parseXmlString = require('../src/parse-xml')
+const chatLib = require('../src/index')
 
-let jsessionid
-let jar = request.jar()
-let eventid = 0
-// events polling interval
-let interval
-let participants = 0
-// automated message sender interval
 let messageInterval
 
-request({
-  jar,
-  url: 'https://sm2-uccx.dcloud.cisco.com/ccp/chat/100000/redirect',
-  resolveWithFullResponse: true,
-  simple: false,
-  followRedirect: false,
-  qs: {
-    author: 'Coty Condry',
-    title: 'Facebook Messenger',
-    extensionField_Name: 'Facebook Messenger',
-    extensionField_Email: '',
-    extensionField_PhoneNumber: '',
-    extensionField_ccxqueuetag: 'Chat_Csq28'
-  }
-}).then(response => {
-  // poll events every 5 seconds
-  interval = setInterval(function () {
-    // check for events for this session
-    request({
-      jar,
-      url: 'https://sm2-uccx.dcloud.cisco.com/ccp/chat',
-      qs: {
-        // jsessionid,
-        eventid,
-        all: true
-      }
-    }).then(events => {
-      // parse xml events string into JSON
-      return parseXmlString(events)
-    }).then(jsonData => {
-      // parse the events in the json
-      parseEvents(jsonData.chatEvents)
-    }).catch(e => {
-      console.log('session expired', e)
-      // clear the current intervals
-      clearInterval(interval)
+const handlers = {
+  onMessageEvent (from, message) {
+    console.log(from, 'said', message)
+  },
+  onStatusEvent (status, detail) {
+    console.log('status event', status, detail)
+  },
+  onPresenceEvent (from, status) {
+    console.log('presence event', from, status)
+  },
+  onPresenceJoined (from) {
+    console.log('presence joined', from)
+    console.log('participant joined. start sending messages as customer.')
+    // check that there is not a current interval set up for sending messages
+    if (messageInterval) {
+      // clear the current interval
       clearInterval(messageInterval)
-    })
-  }, 5000)
-}).catch(e => {
-  console.error('error', e.message)
+    }
+    // set up new interval to send messages to agent every 5 seconds
+    messageInterval = setInterval(() => {
+      this.sendMessage('hi from customer')
+    }, 5000)
+  },
+  onPresenceLeft (from) {
+    console.log('presence left', from)
+  },
+  onLastParticipantLeft () {
+    console.log('last participant left')
+  },
+  onTypingEvent (from, status) {
+    console.log('typing event', from, status)
+  },
+  onTypingStart (from) {
+    console.log(from, 'is typing')
+  },
+  onTypingStop (from) {
+    console.log(from, 'stopped typing')
+  },
+  onOtherEvent (type, ev) {
+    console.log('other event', type, ev)
+  },
+  onAgentTimeout (message) {
+    console.log('agent timeout', message)
+  },
+  onChatCreated () {
+    console.log('chat created')
+  },
+  onStopPolling () {
+    console.log('polling stopped')
+    // clear our automated cutomer message interval
+    clearInterval(messageInterval)
+  },
+  onSessionExpired () {
+    console.log('session expired')
+  }
+}
+
+const chat = new chatLib({
+  urlBase: 'https://sm2-uccx.dcloud.cisco.com/ccp',
+  form: 100000,
+  csq: 'Chat_Csq28',
+  title: 'Facebook Messenger',
+  customerName: 'Facebook Messenger',
+  author: 'Coty Condry',
+  handlers
 })
 
-function parseEvents (events) {
-  // StatusEvent: [ [Object] ],
-  // PresenceEvent: [ [Object] ],
-  // TypingEvent: [ [Object], [Object] ],
-  // MessageEvent: [ [Object], [Object] ]
-  const eventTypes = Object.keys(events)
-  for (const eventType of eventTypes) {
-    for (const ev of events[eventType]) {
-      processEvent(eventType, ev)
-    }
-  }
-}
-
-function processEvent (type, ev) {
-  // console.log('event', ev)
-  // get event ID
-  const id = parseInt(ev.id)
-  if (id > eventid) {
-    // increment eventid
-    eventid = id
-  }
-  if (type === 'MessageEvent') {
-    // message events
-    console.log(ev.from[0], 'said', decodeURIComponent(ev.body[0].replace(/\+/g, ' ')))
-    // console.log(ev)
-  } else if (type === 'StatusEvent') {
-    const status = ev.status[0]
-    // status events
-    switch (status) {
-      case 'chat_timedout_waiting_for_agent': {
-        console.log(ev.detail[0])
-        break
-      }
-      case 'chat_ok': {
-        console.log('Chat request created')
-        break
-      }
-      default: {
-        console.log('status event', ev)
-        break
-      }
-    }
-  } else if (type === 'PresenceEvent') {
-    console.log(`presence event: ${ev.from[0]} ${ev.status[0]}`)
-    // keep track of the number of participants
-    if (ev.status[0] === 'joined') {
-      participants++
-      console.log('participant joined. start sending messages as customer.')
-      // check that there is not a current interval set up for sending messages
-      if (messageInterval) {
-        // clear the current interval
-        clearInterval(messageInterval)
-      }
-      // set up new interval to send messages to agent every 5 seconds
-      messageInterval = setInterval(function () {
-        // send a message as the customer
-        request({
-          jar,
-          method: 'PUT',
-          url: 'https://sm2-uccx.dcloud.cisco.com/ccp/chat',
-          headers: {
-            'Content-Type': 'application/xml'
-          },
-          body: '<Message><body>hi from customer</body></Message>'
-        }).then(response => {
-          console.log('customer message sent.')
-        }).catch(e => {
-          console.error('failed to send message as customer', e.message)
-        })
-      }, 5000)
-    } else if (ev.status[0] === 'left') {
-      participants--
-      // if the last participant left, the chat has ended. stop polling.
-      if (participants < 1) {
-        console.log('last participant left chat. stop polling.')
-        // clear the current intervals
-        clearInterval(interval)
-        clearInterval(messageInterval)
-      }
-    }
-    console.log('participants =', participants)
-  } else if (type === 'TypingEvent') {
-    console.log(`typing event: ${ev.from[0]} ${ev.status[0]}`)
-  } else {
-    console.log(type, ev)
-  }
-}
+chat.start()
